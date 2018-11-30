221285106090786bdf01ae966626da3138df8033
Srijal6 <40156467+Srijal6@users.noreply.github.com>
2018-11-30 19:52:44 +0800

    Update CAD.py

--- CAD.py
+++ CAD.py
@@ -1,18 +1,17 @@
-
 from __future__ import print_function
-#import argparse
-#import torch
-# import tensorflow as tf
-#import tf.nn as F
-#import torch.optim as optim
-import model
-#from torch.autograd import Variable
+import argparse
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 import os
 import math
 import data_loader
-#import ResNet as models
+import ResNet as models
 from torch.utils import model_zoo
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+import tensorflow as tf
 
 # Training settings
 batch_size = 32
@@ -27,13 +26,13 @@ root_path = "./dataset/"
 source_name = "amazon"
 target_name = "webcam"
 
-cuda = not no_cuda and tf.test.is_gpu_available()
-
-tf.random.set_random_seed(seed)
+cuda = not no_cuda and tf.device()
+tf.device(seed)
 if cuda:
-    tf.device(seed)  #tf.matmul(seed)
+    torch.cuda.manual_seed(seed)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+#def source_loader(root_path, source_name, batch_size, kwargs)
 
 source_loader = data_loader.load_training(root_path, source_name, batch_size, kwargs)
 target_train_loader = data_loader.load_training(root_path, target_name, batch_size, kwargs)
@@ -44,13 +43,10 @@ len_target_dataset = len(target_test_loader.dataset)
 len_source_loader = len(source_loader)
 len_target_loader = len(target_train_loader)
 
-def load_pretrain(model):
-   # saver = tf.train.import_meta_graph('my_test_model-1000.meta')
-    #https://cv-tricks.com/tensorflow-tutorial/save-restore-tensorflow-models-quick-complete-tutorial/
-    #have to add my trained model
-    with tf.Session() as sess:
-        new_saver = tf.train.import_meta_graph('my_test_model-1000.meta')
-        new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+    def load_pretrain(model):
+        dir = 'example'
+        pretrained_dict = model_zoo.load_url(dir)
+    model_dict = model.state_dict()
     for k, v in model_dict.items():
         if not "cls_fc" in k:
             model_dict[k] = pretrained_dict[k[k.find(".") + 1:]]
@@ -59,56 +55,59 @@ def load_pretrain(model):
 
 def train(epoch, model):
     LEARNING_RATE = lr / tf.math.pow((1 + 10 * (epoch - 1) / epochs), 0.75)
-    print('learning rate{: .4f}'.format(LEARNING_RATE) )
-    optimizer = tf.train.Optimizer([
+    print('learning rate{: .4f}'.format(LEARNING_RATE))
+    optimizer = tf.train.GradientDescentOptimizer([
         {'params': model.sharedNet.parameters()},
-        {'params': model.cls_fc.parameters(), 'lr': LEARNING_RATE},
-        ], lr=LEARNING_RATE / 10, momentum=momentum, weight_decay=l2_decay)
+        {'params': model.cls_fc.parameters(), 'lr': LEARNING_RATE}
+        ], lr)
 
-    tf.train()
+   # model.train()
 
-    iter_source = iter(source_loader)
-    iter_target = iter(target_train_loader)
-    num_iter = len_source_loader
-    for i in range(1, num_iter):
-        data_source, label_source = iter_source.next()
-        data_target, _ = iter_target.next()
-        if i % len_target_loader == 0:
-            iter_target = iter(target_train_loader)
-        if cuda:
-            data_source, label_source = data_source.cuda(), label_source.cuda() # tf.matmul(data_source) 
-            data_target = data_target.cuda() #tf.matmul(data_target)
-        data_source, label_source = Variable(data_source), Variable(label_source)
-        data_target = Variable(data_target)
-        # zero the parameter gradient
-        # optimizer.zero_grad()  #Tensorflow already have it zero as default
+ with tf.Session() as sess:
+     sess.run(tf.global_variables_initializer())
+     iter_source = iter(source_loader)
+     iter_target = iter(target_train_loader)
+     num_iter = len_source_loader
+     for i in range(1, num_iter):
+         data_source, label_source = iter_source.next()
+         data_target, _ = iter_target.next()
+         if i % len_target_loader == 0:
+             iter_target = iter(target_train_loader)
+         if cuda:
+             data_source, label_source = data_source.cuda(), label_source.cuda()
+             data_target = data_target.cuda()
+         data_source, label_source = tf.Variable(data_source), tf.Variable(label_source)
+         data_target = Variable(data_target)
+         sess.run(optimizer, feed_dict={x: data_source, y: label_source, keep_prob: 0.7})
+
+        optimizer.zero_grad()
         label_source_pred, loss_mmd = model(data_source, data_target)
-        loss_cls = tf.losses.sparse_softmax_cross_entropy(label_source_pred, dim=1), label_source)
+        loss_cls = F.nll_loss(F.log_softmax(label_source_pred, dim=1), label_source)
         gamma = 2 / (1 + math.exp(-10 * (epoch) / epochs)) - 1
         loss = loss_cls + gamma * loss_mmd
-        
-       
+        loss.backward()
+        optimizer.step()
         if i % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tsoft_Loss: {:.6f}\tmmd_Loss: {:.6f}'.format(
                 epoch, i * len(data_source), len_source_dataset,
-                100. * i / len_source_loader, loss.data[0], loss_cls.data[0], loss_mmd.data[0])
-            
-            
+                100. * i / len_source_loader, loss.data[0], loss_cls.data[0], loss_mmd.data[0]))
+
+    # with tf.Session() as sess:
+    #    sess.run(tf.stack((model[:,0] + 1, x[:, 1]), axis=1))
 def test(model):
-    tf.estimator.Estimator()   #is it correct
+    model.eval()
     test_loss = 0
     correct = 0
 
     for data, target in target_test_loader:
         if cuda:
-            data, target = tf.matmul(data), tf.matmul(target) #data.device
+            data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
         s_output, t_output = model(data, data)
-        test_loss += tf.nn.sparse_softmax_cross_entropy_with_logits((s_output, dim = 1), target, size_average=False).data[0] # sum up batch loss
-        pred = tf.math.log(s_output(1)[1]) # get the index of the max log-probability
+        test_loss += F.nll_loss(F.log_softmax(s_output, dim = 1), target, size_average=False).data[0] # sum up batch loss
+        pred = s_output.data.max(1)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-                  
-                  
+
     test_loss /= len_target_dataset
     print('\n{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         target_name, test_loss, correct, len_target_dataset,
@@ -117,11 +116,11 @@ def test(model):
 
 
 if __name__ == '__main__':
-    model = models.model(num_classes=31)
+    model = models.DANNet(num_classes=31)
     correct = 0
     print(model)
     if cuda:
-        model.device()  #tf.matmul(model)
+        model.cuda()
     model = load_pretrain(model)
     for epoch in range(1, epochs + 1):
         train(epoch, model)
